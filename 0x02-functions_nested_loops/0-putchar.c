4391d491dbe22859aac3602a291619b8100d1cfd
PaulNwams <paulnwamanna@example.com>
2023-01-09 00:45:32 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description - prints '_putchar'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+	c = '_putchar';
+
+	putchar(c);
+	return (0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+C Project: Argument to main Function

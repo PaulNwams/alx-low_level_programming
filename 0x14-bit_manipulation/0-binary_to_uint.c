59b7abd98778923eafa590b264e5605449856f31
PaulNwams <paulnwamanna@example.com>
2023-02-28 02:31:20 -0800

    tasks bits

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,8 @@
 /**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: A pointer to a string of 0 and 1 chars
- * Return: If b is NULL or contains chars not 0 or 1 
+ * Return: If b is NULL or contains chars not 0 or 1
+ * otherwise - the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
@@ -11,7 +12,7 @@ unsigned int binary_to_uint(const char *b)
 	int len = 0;
 
 	if (b[len] == '\0')
-		retur (0);
+		return (0);
 
 	while (b[len] == '0') || (b[len] == '1'))
 	{
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,6 +3,7 @@
 /**
  * set_bit - sets the value of a bit at a given index to 1
  * @n: A pointer to the bit.
+ * @index: the index to set the value at - indices start at 0.
  * Return: if an error occurs - -1.
  * otherwise -1
  */
